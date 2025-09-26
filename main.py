e73bde7ad005d4df5f6f0f4a514a4cca2243eaf6
tgeng <tgeng@uncfsu.edu>
2025-09-26 10:35:33 -0400

    main.py template

---
+++ main.py
@@ -0,0 +1,9 @@
+game_name = "" # TODO, name your game project Python
+print() # TODO, print greetings with game name
+print("======================") # TODO, add or remove equal symbol to align the greetings
+
+# Ask for the character's name
+name = input()
+
+# Print the name
+print()
